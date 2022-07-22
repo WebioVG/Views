1b412edaa1e41d8dc33ab93ad5d2f7f4a77581b0
webiovg <kevin.kowalsky1@gmail.com>
2022-07-22 11:47:07 +0200

    done: display the number of views for each note

--- index.php
+++ index.php
@@ -24,7 +24,44 @@
         $errors = [];
         $success = false;
 
-        $views = [];
+        $views = [
+            [
+                'name' => 'Kevin',
+                'message' => 'Mon premier commentaire',
+                'note' => '3',
+                'created_at' => date('Y-m-d H:i:s', strtotime('+2 hours'))
+            ],
+            [
+                'name' => 'Kevin',
+                'message' => 'Mon second commentaire',
+                'note' => '3',
+                'created_at' => date('Y-m-d H:i:s', strtotime('+2 hours'))
+            ],
+            [
+                'name' => 'Kevin',
+                'message' => 'Mon troisième commentaire',
+                'note' => '1',
+                'created_at' => date('Y-m-d H:i:s', strtotime('+2 hours'))
+            ],
+            [
+                'name' => 'Toto',
+                'message' => 'Lorem ipsum',
+                'note' => '5',
+                'created_at' => date('Y-m-d H:i:s', strtotime('+2 hours'))
+            ],
+            [
+                'name' => 'Titi',
+                'message' => 'Lorem ipsum',
+                'note' => '2',
+                'created_at' => date('Y-m-d H:i:s', strtotime('+2 hours'))
+            ],
+            [
+                'name' => 'Tutu',
+                'message' => 'Lorem ipsum dolor',
+                'note' => '3',
+                'created_at' => date('Y-m-d H:i:s', strtotime('+2 hours'))
+            ],
+        ];
 
         if (isSubmit()) {
             if (strlen($name) === 0) {
@@ -49,10 +86,19 @@
                     'created_at' => date('Y-m-d H:i:s', strtotime('+2 hours'))
                 ]);
             }
+        }
 
-            function displayCurrentTime($date) {
-                return date('l d F Y \à H\-i', strtotime($date));
-            }
+        function filterViewsByNote($views, $note) {
+            return array_filter($views, function ($v) use ($note) {
+                return ($v['note'] == $note);
+            });
+        }
+
+        function getPercentageOfViewsByNote($views, $note) {
+            $total = count($views);
+            $nbOfNotes = count(filterViewsByNote($views, $note));
+
+            return $nbOfNotes / $total * 100;
         }
     ?>
 
@@ -104,7 +150,67 @@
             </div>
     
             <div class="w-1/3">
-    
+                <div class="mb-1 w-full flex">
+                    <div class="w-[15%] flex">
+                        <span class="mr-1">5</span>
+                        <svg fill="#ff0" width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                            <!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                            <path d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z"/>
+                        </svg>
+                    </div>
+                    <div class="w-[70%] bg-slate-200 rounded">
+                        <div class="h-full bg-yellow-400 w-[<?= getPercentageOfViewsByNote($views, 5) ?>%]"></div>
+                    </div>
+                    <div class="w-[15%] ml-3">(<?= count(filterViewsByNote($views, 5)) ?>)</div>
+                </div>
+                <div class="mb-1 w-full flex">
+                    <div class="w-[15%] flex">
+                        <span class="mr-1">4</span>
+                        <svg fill="#ff0" width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                            <path d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z"/>
+                        </svg>
+                    </div>
+                    <div class="w-[70%] bg-slate-200 rounded">
+                        <div class="h-full bg-yellow-400 w-[<?= getPercentageOfViewsByNote($views, 4) ?>%]"></div>
+                    </div>
+                    <div class="w-[15%] ml-3">(<?= count(filterViewsByNote($views, 4)) ?>)</div>
+                </div>
+                <div class="mb-1 w-full flex">
+                    <div class="w-[15%] flex">
+                        <span class="mr-1">3</span>
+                        <svg fill="#ff0" width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                            <path d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z"/>
+                        </svg>
+                    </div>
+                    <div class="w-[70%] bg-slate-200 rounded">
+                        <div class="h-full bg-yellow-400 w-[<?= getPercentageOfViewsByNote($views, 3) ?>%]"></div>
+                    </div>
+                    <div class="w-[15%] ml-3">(<?= count(filterViewsByNote($views, 3)) ?>)</div>
+                </div>
+                <div class="mb-1 w-full flex">
+                    <div class="w-[15%] flex">
+                        <span class="mr-1">2</span>
+                        <svg fill="#ff0" width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                            <path d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z"/>
+                        </svg>
+                    </div>
+                    <div class="w-[70%] bg-slate-200 rounded">
+                        <div class="h-full bg-yellow-400 w-[<?= getPercentageOfViewsByNote($views, 2) ?>%]"></div>
+                    </div>
+                    <div class="w-[15%] ml-3">(<?= count(filterViewsByNote($views, 2)) ?>)</div>
+                </div>
+                <div class="mb-1 w-full flex">
+                    <div class="w-[15%] flex">
+                        <span class="mr-1">1</span>
+                        <svg fill="#ff0" width="25" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                            <path d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z"/>
+                        </svg>
+                    </div>
+                    <div class="w-[70%] bg-slate-200 rounded">
+                        <div class="h-full bg-yellow-400 w-[<?= getPercentageOfViewsByNote($views, 1) ?>%]"></div>
+                    </div>
+                    <div class="w-[15%] ml-3">(<?= count(filterViewsByNote($views, 1)) ?>)</div>
+                </div>
             </div>
     
             <div class="w-1/3 text-center">
@@ -146,25 +252,23 @@
         </form>
     </section>
 
-    
-
-            <?php foreach($views as $view) { ?>
-                <section class="mb-4">
-                    <div class="flex">
-                        <div class="w-[100px]">
-                            <div class="bg-yellow-400 flex justify-center items-center rounded-[50%] w-[70px] h-[70px] text-white text-lg"><?= strtoupper(substr($view['name'], 0, 1)) ?></div>
-                        </div>
-                        <div class="w-full border rounded">
-                            <p class="bg-slate-200 pl-2 py-1"><?= $view['name'] ?></p>
-                            <div class="pl-2 pt-2 pb-4">
-                                <div><?= $view['note'] ?> / 5</div>
-                                <p><?= $view['message'] ?></p>
-                            </div>
-                            <p class="text-end bg-slate-200 pl-2 py-1 border"><?= dateToFrench($view['created_at'], 'l d F Y \à H\hi') ?></p>
-                        </div>
+    <?php foreach($views as $view) { ?>
+        <section class="mb-4">
+            <div class="flex">
+                <div class="w-[100px]">
+                    <div class="bg-yellow-400 flex justify-center items-center rounded-[50%] w-[70px] h-[70px] text-white text-lg"><?= strtoupper(substr($view['name'], 0, 1)) ?></div>
+                </div>
+                <div class="w-full border rounded">
+                    <p class="bg-slate-200 pl-2 py-1"><?= $view['name'] ?></p>
+                    <div class="pl-2 pt-2 pb-4">
+                        <div><?= $view['note'] ?> / 5</div>
+                        <p><?= $view['message'] ?></p>
                     </div>
-            </section>
-            <?php } ?>
+                    <p class="text-end bg-slate-200 pl-2 py-1 border"><?= dateToFrench($view['created_at'], 'l d F Y \à H\hi') ?></p>
+                </div>
+            </div>
+    </section>
+    <?php } ?>
 
 </body>
 
